7e3d14dafe2e76962c95584991fc63d62e1dabff
Amro Alnajar <amro.alnajar92@gmail.com>
2022-06-18 02:00:18 +0200

    update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace economyopedia_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("WakeMe")]
+        public IActionResult WakeMe()
+        {
+            return Ok();
+        }
+    }
+}
